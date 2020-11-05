9d51fc95555321ab71141acf48044fff9a68e7da
vickeycheung <13560328059@163.com>
2020-11-05 15:20:58 +0800

    first commit

---
+++ src/main/java/com/youcash/ucff/verifydf/DfMain.java
@@ -0,0 +1,18 @@
+package com.youcash.ucff.verifydf;
+
+public class DfMain {
+
+	public void justDoSthWithTest(){
+		int a = 1;
+		int b = a;
+		
+		System.out.println(a + b);
+		
+	}
+	
+	public static void main(String[] args) {
+		DfMain df = new DfMain();
+		df.justDoSthWithTest();
+	}
+
+}
---
+++ src/test/java/com/youcash/ucff/verifydf/DfMainTest.java
@@ -0,0 +1,12 @@
+package com.youcash.ucff.verifydf;
+
+import org.junit.Test;
+
+public class DfMainTest {
+	
+	@Test
+	public void dosthTest(){
+		DfMain df = new DfMain();
+		df.justDoSthWithTest();
+	}
+}
